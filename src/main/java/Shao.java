4c4073e6e57f8b0424329394a8567c2b24fbe182
leeshaowee <lee_shao_wee@hotmail.com>
2023-01-14 09:41:48 +0800

    Change file name from Duke to Shao

--- src/main/java/Shao.java
+++ src/main/java/Shao.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Duke {
+public class Shao {
     public static void printRowLine() {
         println("_________________________________________");
     }
